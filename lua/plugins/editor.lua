cba7c536b0f357ce6bcc71607fc3e81f47cbbc34
Timothy Mertz <mertzt89@gmail.com>
2023-06-11 14:02:02 -0500

    Fix grep operator

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,11 @@
 return {
   { "mg979/vim-visual-multi", lazy = false },
   { "numToStr/Comment.nvim", config = true, event = "VeryLazy" },
-  { "grep_operator", name = "grep_operator", dir = "local_plugins/grep_operator", config = true, lazy = false },
+  {
+    "grep_operator",
+    name = "grep_operator",
+    dir = vim.fn.stdpath("config") .. "/local_plugins/grep_operator",
+    config = true,
+    lazy = false,
+  },
 }
