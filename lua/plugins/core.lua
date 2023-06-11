7fabc7160da1635e745c3517c6017f71b49c6927
Timothy Mertz <mertzt89@gmail.com>
2023-06-10 22:53:02 -0500

    telescope: Add fzf native sorter

---
+++ lua/plugins/core.lua
@@ -0,0 +1,13 @@
+return {
+  -- Telescope FZF Native Sorter
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+}
