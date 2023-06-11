a9eedb56647e0a748cb710a476288dab1c55d9cc
Timothy Mertz <mertzt89@gmail.com>
2023-06-11 17:05:36 -0500

    Replace custom trim() with vim.trim()

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -1,16 +1,12 @@
 local Plugin = { "hrsh7th/nvim-cmp" }
 
-local function trim(s)
-  return (s:gsub("^%s*(.-)%s*$", "%1"))
-end
-
 -- Trim LSP responses for consistent formatting.
 -- (clangd sometimes includes whitepsace around item.abbr)
 Plugin.opts = function(_, opts)
   local orig_fn = opts.formatting.format
   opts.formatting.format = function(cmp_entry, cmp_item)
     local item = orig_fn(cmp_entry, cmp_item)
-    item.abbr = trim(item.abbr)
+    item.abbr = vim.trim(item.abbr)
     return item
   end
   return opts
