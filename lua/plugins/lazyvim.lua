e6000ed5a21bc101cf82bf17ba52fec172fb942f
Timothy Mertz <mertzt89@gmail.com>
2023-06-11 14:47:20 -0500

    Customize lsp icons

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -2,5 +2,10 @@ return {
   "LazyVim/LazyVim",
   opts = {
     colorscheme = "tokyonight-night",
+    icons = {
+      kinds = vim.tbl_map(function(icon)
+        return icon .. " "
+      end, require("config.icons").lspkind),
+    },
   },
 }
--- lua/plugins/ui/symbols-outline.lua
+++ lua/plugins/ui/symbols-outline.lua
@@ -1,6 +1,11 @@
-return {
-  "simrat39/symbols-outline.nvim",
-  cmd = "SymbolsOutline",
-  keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-  config = true,
-}
+local Plugin = { "simrat39/symbols-outline.nvim" }
+
+local symbols = vim.tbl_map(function(icon)
+  return { icon = icon }
+end, require("config.icons").lspkind)
+
+Plugin.opts = { symbols = symbols }
+Plugin.cmd = "SymbolsOutline"
+Plugin.keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } }
+
+return Plugin
