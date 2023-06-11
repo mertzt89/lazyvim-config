61b7bc47090c71da78413ae1fe27a45d3a3aacb0
Timothy Mertz <mertzt89@gmail.com>
2023-06-10 23:56:47 -0500

    local plugin: Add grep operator

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,5 @@
 return {
   { "mg979/vim-visual-multi", lazy = false },
   { "numToStr/Comment.nvim", config = true, event = "VeryLazy" },
+  { "grep_operator", name = "grep_operator", dir = "local_plugins/grep_operator", config = true, lazy = false },
 }
