24d17134a975c23d72ed166724d0521594f787da
Timothy Mertz <mertzt89@gmail.com>
2023-06-12 01:44:08 -0500

    plugin: Add overseer.nvim

---
+++ lua/plugins/editor/overseer.lua
@@ -0,0 +1,21 @@
+return {
+  "stevearc/overseer.nvim",
+  opts = {
+    task_list = {
+      bindings = {
+        ["<C-l>"] = false,
+        ["<C-h>"] = false,
+        ["q"] = "<cmd>OverseerClose<CR>",
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader><CR>",
+      "<cmd>OverseerRun<CR>",
+    },
+    { "<leader>oo", "<cmd>OverseerToggle<CR>" },
+    { "<leader>oa", "<cmd>OverseerQuickAction<CR>" },
+    { "<leader>of", "<cmd>OverseerQuickAction open float<CR>" },
+  },
+}
