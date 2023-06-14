f181d5ac60738dbda95aeca6cd6e6aa43a91d727
Timothy Mertz <mertzt89@gmail.com>
2023-06-14 16:26:31 -0500

    plugin: mini.indentscope: Disable animation / config hl

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,6 +2,9 @@ return {
   "folke/tokyonight.nvim",
   opts = {
     on_highlights = function(hl, c)
+      --
+      -- Borderless Telescope Window
+      --
       local prompt = "#2d3149"
       hl.TelescopeNormal = {
         bg = c.bg_dark,
@@ -30,8 +33,15 @@ return {
         bg = c.bg_dark,
         fg = c.bg_dark,
       }
+
+      -- Instead of showing the current indent scope with blue,
+      -- use a slightly brightened version IndentBlanklineChar
+      hl.MiniIndentscopeSymbol = {
+        fg = require("tokyonight.util").lighten(hl.IndentBlanklineChar.fg, 0.8),
+      }
     end,
 
+    -- Default to "night" i.e. `:colorscheme tokyonight-night`
     style = "night",
   },
 }
