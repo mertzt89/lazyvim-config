ab4d9c526307d5bcc9922de2276184c3f63fc098
Timothy Mertz <mertzt89@gmail.com>
2023-06-22 18:57:20 -0500

    dap: keymap: <leader>dn to Step Over

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,3 +1,15 @@
 return {
+  {
+    "mfussenegger/nvim-dap",
+    keys = {
+      {
+        "<leader>dn",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step Over",
+      },
+    },
+  },
   require("util.mason").add_ensure_installed("cpptools"),
 }
