b32b2b6c1750c35f142d5cfb03e6fbc5d53544d1
Timothy Mertz <mertzt89@gmail.com>
2023-06-29 14:13:34 -0500

    plugins: extras: Add mini-files

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
   { import = "lazyvim.plugins.extras.ui.edgy" },
   { import = "lazyvim.plugins.extras.dap.core" },
+  { import = "lazyvim.plugins.extras.editor.mini-files" },
   { import = "plugins.editor" },
   { import = "plugins.lsp" },
   { import = "plugins.ui" },
