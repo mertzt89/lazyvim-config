fe4ade3b2e17198e091edf3af7acae01841f83fe
Timothy Mertz <mertzt89@gmail.com>
2023-06-29 14:13:44 -0500

    plugins: extras: Add yanky

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,6 +2,7 @@ return {
   { import = "lazyvim.plugins.extras.ui.edgy" },
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },
+  { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "plugins.editor" },
   { import = "plugins.lsp" },
   { import = "plugins.ui" },
