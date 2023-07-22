9f0f68934fd6e00b0b1a2c78f344cb184cec05df
Timothy Mertz <mertzt89@gmail.com>
2023-07-22 11:48:56 -0500

    cleanup: Move lazyvim extras to seperate file

--- lua/plugins/lazyvim_extras/init.lua
+++ lua/plugins/lazyvim_extras/init.lua
@@ -1,3 +1,7 @@
 return {
+  { import = "lazyvim.plugins.extras.coding.yanky" },
+  { import = "lazyvim.plugins.extras.dap.core" },
+  { import = "lazyvim.plugins.extras.editor.mini-files" },
   { import = "lazyvim.plugins.extras.lang.cmake" },
+  { import = "lazyvim.plugins.extras.ui.edgy" },
 }
