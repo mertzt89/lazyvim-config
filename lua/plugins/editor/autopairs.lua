a78dc577a7a34bbc714ecec806ab9c702bb65caa
Timothy Mertz <mertzt89@gmail.com>
2023-08-03 12:02:51 -0500

    Replace mini.pairs with nvim-autopairs

---
+++ lua/plugins/editor/autopairs.lua
@@ -0,0 +1,11 @@
+return {
+  -- disable mini.pairs in favor of nvim-autopairs
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+  {
+    'windwp/nvim-autopairs',
+    config = true
+  },
+}
