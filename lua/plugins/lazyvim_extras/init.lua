222b8566e5d764f001f38fe826d50eeb4cbc0c3a
Timothy Mertz <mertzt89@gmail.com>
2023-08-03 12:03:01 -0500

    Add clangd extra

--- lua/plugins/lazyvim_extras/init.lua
+++ lua/plugins/lazyvim_extras/init.lua
@@ -2,6 +2,7 @@ return {
   { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },
+  { import = "lazyvim.plugins.extras.lang.clangd" },
   { import = "lazyvim.plugins.extras.lang.cmake" },
   { import = "lazyvim.plugins.extras.ui.edgy" },
 }
