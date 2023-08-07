d6b0d25e2d3f2d8086f82f10b61d2d42a11ea8ab
Timothy Mertz <mertzt89@gmail.com>
2023-08-06 23:13:16 -0500

    Add copilot example

---
+++ lua/plugins/lazyvim_extras/copilot.lua
@@ -0,0 +1,13 @@
+return {}
+
+-- uncomment to enable.. not enabled by default
+-- return {
+--   { import = "lazyvim.plugins.extras.coding.copilot" },
+--   {
+--     "zbirenbaum/copilot.lua",
+--     opts = {
+--       suggestion = { enabled = true },
+--       panel = { enabled = true }
+--     }
+--   }
+-- }
