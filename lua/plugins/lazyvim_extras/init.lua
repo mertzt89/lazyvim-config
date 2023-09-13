ad08408b01973a48d33c011181c717b574ff3017
Timothy Mertz <mertzt89@gmail.com>
2023-09-12 19:12:35 -0500

    feat(clangd): Make cmd configurable via neoconf

--- lua/plugins/lazyvim_extras/init.lua
+++ lua/plugins/lazyvim_extras/init.lua
@@ -2,7 +2,6 @@ return {
   { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },
-  { import = "lazyvim.plugins.extras.lang.clangd" },
   { import = "lazyvim.plugins.extras.lang.cmake" },
   { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.ui.edgy" },
---
+++ lua/plugins/lsp/clangd.lua
@@ -0,0 +1,35 @@
+local defaults = {
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--function-arg-placeholders",
+    "--fallback-style=llvm",
+  }
+}
+
+-- Neoconf integration
+require("neoconf.plugins").register({
+  name = "clangd",
+  on_schema = function(schema)
+    -- this call will create a json schema based on the lua types of your default settings
+    schema:import("clangd", defaults)
+  end,
+})
+
+return {
+  { import = "lazyvim.plugins.extras.lang.clangd" },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        clangd = function(_, opts)
+          local conf = require('neoconf').get("clangd", defaults)
+          opts.cmd = conf.cmd
+        end
+      },
+    },
+  },
+}
