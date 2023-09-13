f4dec08367f2fb8a54022d96becb5bf26248e21d
Timothy Mertz <mertzt89@gmail.com>
2023-09-12 19:14:36 -0500

    fix(textobjects): Extend custom object

--- lua/plugins/editor/textobjects.lua
+++ lua/plugins/editor/textobjects.lua
@@ -18,13 +18,13 @@ Plugin.dependencies = {
   {
     "echasnovski/mini.ai",
     dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
-    opts = function()
+    opts = function(opts, _)
       local gen_spec = require("mini.ai").gen_spec
-      return {
+      return vim.tbl_deep_extend("force", opts, {
         custom_textobjects = {
           F = gen_spec.treesitter({ a = { "@function.documented" }, i = "@function.inner" }),
-        },
-      }
+        }
+      })
     end,
   },
 }
