554f4827544bb8888474df743cc7f0eea034ac45
Timothy Mertz <mertzt89@gmail.com>
2023-09-12 19:14:07 -0500

    feat(autopairs): Try ultimate-autopair.nvim

--- lua/plugins/editor/autopairs.lua
+++ lua/plugins/editor/autopairs.lua
@@ -1,11 +1,22 @@
 return {
-  -- disable mini.pairs in favor of nvim-autopairs
   {
     "echasnovski/mini.pairs",
     enabled = false,
   },
   {
     'windwp/nvim-autopairs',
-    config = true
+    config = true,
+    enabled = false,
+  },
+  {
+    'altermo/ultimate-autopair.nvim',
+    event = { 'InsertEnter', 'CmdlineEnter' },
+    branch = 'v0.6',
+    config = function()
+      local opts = require("ultimate-autopair.default").conf
+      local extra_pairs = { { '/*', '*/', fly = true, dosurround = true, newline = true, space = true } }
+      opts.internal_pairs = vim.tbl_deep_extend("force", opts.internal_pairs, extra_pairs)
+      require("ultimate-autopair").setup(opts)
+    end
   },
 }
