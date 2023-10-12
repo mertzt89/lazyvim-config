c8a9526596034fd31507beb0d2a649d1bd294b73
Timothy Mertz <mertzt89@gmail.com>
2023-10-12 10:57:01 -0500

    feat!(LazyVim): Update to v10.0.0

--- lua/plugins/lsp/python.lua
+++ lua/plugins/lsp/python.lua
@@ -1,24 +1,4 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      setup = {
-        servers = {
-          pylsp = {},
-        },
-      },
-    },
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      vim.list_extend(opts.sources, {
-        nls.builtins.formatting.black
-      })
-    end,
-  },
-  require("util.mason").add_ensure_installed("python-lsp-server"),
-  require("util.mason").add_ensure_installed("black"),
-  require("util.mason").add_ensure_installed("flake8"),
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.formatting.black" },
 }
